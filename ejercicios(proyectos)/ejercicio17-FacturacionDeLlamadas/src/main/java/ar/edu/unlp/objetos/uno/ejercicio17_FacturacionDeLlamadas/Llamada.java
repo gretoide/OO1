5333f828af4f9af7008e5343245e79f23d97d9c9
gretoide <gretaxpiacentini@gmail.com>
2022-11-18 15:25:21 -0300

    update

---
+++ ejercicios(proyectos)/ejercicio17-FacturacionDeLlamadas/src/main/java/ar/edu/unlp/objetos/uno/ejercicio17_FacturacionDeLlamadas/Llamada.java
@@ -0,0 +1,37 @@
+package ar.edu.unlp.objetos.uno.ejercicio17_FacturacionDeLlamadas;
+
+import java.time.LocalDateTime;
+
+public abstract class Llamada {
+	private int emisor;
+	private int receptor; 
+	private int duracion;
+	private LocalDateTime comienzo;
+	
+	public Llamada(LocalDateTime comienzo, int duracion, int emisor, int receptor) {
+		super();
+		this.emisor = emisor;
+		this.receptor = receptor;
+		this.duracion = duracion;
+		this.comienzo = comienzo;
+	}
+	
+	
+	
+	public LocalDateTime getComienzo() {
+		return comienzo;
+	}
+	
+	public boolean lapso(LocalDateTime comienzo, LocalDateTime fin) {
+		return this.getComienzo().isAfter(comienzo) || this.getComienzo().isBefore(fin) ||
+				this.getComienzo().isEqual(comienzo) || this.getComienzo().isEqual(fin);
+	}
+	
+	protected double calcularCosto(double monto) {
+		return duracion*monto;
+	}
+	
+	
+	abstract double calcularCosto();
+}
+
---
+++ ejercicios(proyectos)/ejercicio17-FacturacionDeLlamadas/src/main/java/ar/edu/unlp/objetos/uno/ejercicio17_FacturacionDeLlamadas/LlamadaInternacional.java
@@ -0,0 +1,34 @@
+package ar.edu.unlp.objetos.uno.ejercicio17_FacturacionDeLlamadas;
+
+import java.time.LocalDateTime;
+
+public class LlamadaInternacional extends Llamada {
+	private String paisOrigen;
+	private String paisDestino;
+	
+	
+	public LlamadaInternacional(LocalDateTime comienzo, int duracion, int emisor, int receptor,String paisOrigen, String paisDestino) {
+		super(comienzo,duracion,emisor,receptor);
+		this.paisOrigen = paisOrigen;
+		this.paisDestino = paisDestino;
+	}
+	public String getPaisOrigen() {
+		return paisOrigen;
+	}
+	public String getPaisDestino() {
+		return paisDestino;
+	}
+	
+	/*si la llamada es diurna (entre las 8am y las 20pm) retorna 4, sino, retorna 3*/
+	public double calcularCosto() {
+		if((this.getComienzo().getHour() > 8 && this.getComienzo().getMinute() > 0)&&
+				(this.getComienzo().getHour() < 20 && this.getComienzo().getMinute() > 0)) {
+			return this.calcularCosto(4);
+		}
+		else {
+			return this.calcularCosto(3);
+		}
+		}
+	
+	
+}
